62696e3d5d596fb357da630415e1a1a2f9a46f27
William Fecteau <william-fecteau@hotmail.com>
2022-11-05 14:57:29 -0400

    Monkey

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
 
+const loader = new THREE.TextureLoader();
+const texture = loader.load('assets/monke.jpg');
+
 var scene = new THREE.Scene();
 var camera = new THREE.OrthographicCamera(WIDTH / -2, WIDTH / 2, HEIGHT / 2, HEIGHT / -2, 1, 1000);
 camera.position.z = 1;
@@ -9,8 +12,10 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(WIDTH, HEIGHT);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(100, 100, 0);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var geometry = new THREE.BoxGeometry(WIDTH, HEIGHT, 0);
+var material = new THREE.MeshBasicMaterial({
+    map: texture,
+});
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
