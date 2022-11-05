7cd558a56c08b240dcf8322a376fafb0c2d5fe17
William Fecteau <william.fecteau.1@ulaval.ca>
2022-11-05 14:32:42 -0400

    Setting up eslintrc.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,22 @@
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
+WIDTH = 32;
 
 var scene = new THREE.Scene();
 var camera = new THREE.OrthographicCamera(WIDTH / -2, WIDTH / 2, HEIGHT / 2, HEIGHT / -2, 1, 1000);
+camera.position.z = 1;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(WIDTH, HEIGHT);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(100, 100, 100);
+var geometry = new THREE.BoxGeometry(100, 100, 0);
 var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 200;
-
 var animate = function () {
     requestAnimationFrame(animate);
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 };
